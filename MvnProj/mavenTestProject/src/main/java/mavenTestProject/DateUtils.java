5e697d23a72e9400f9acad5540fe6746df44e06d
robblis <rblischke@esri.com>
2016-05-23 14:17:37 -0700

    Initial check in

---
+++ MvnProj/mavenTestProject/src/main/java/mavenTestProject/DateUtils.java
@@ -0,0 +1,16 @@
+package mavenTestProject;
+
+import java.util.Date;
+public class DateUtils {
+
+	public static void main(String[] args) {
+		
+		System.out.println(getLocalCurrentDate());
+
+	}
+	
+	private static Date getLocalCurrentDate() {
+		return new Date();
+	}
+
+}
