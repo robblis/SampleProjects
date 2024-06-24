c76d395011cc6166a0e12fdfa26b4343c5602e33
Robert Blischke <rblischke@esri.com>
2024-06-24 12:25:34 -0700

    Initial check in

---
+++ RestProSOE1/RestProSOE1/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RestProSOE1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestProSOE1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ef50865-2eea-4d69-b66e-90b414cb62f2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: ESRI.Server.SOESupport.AddInPackage("RestProSOE1", "2f895aac-d955-4ceb-bf06-2244f78bc3d8",
+    Author = "robe4677",
+    Company = "",
+    Date = "5/16/2024 9:19:55 AM",
+    Description = "",
+    TargetProduct = "Server",
+    TargetVersion = "11.0",
+    Version = "1.0")]
---
+++ RestProSOE1/RestProSOE1/RestProSOE1.cs
@@ -0,0 +1,156 @@
+﻿// Copyright 2018 ESRI
+// 
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+// 
+// You may freely redistribute and use this sample code, with or
+// without modification, provided you include the original copyright
+// notice and use restrictions.
+// 
+// See the use restrictions at <your Enterprise SDK install location>/userestrictions.txt.
+// 
+
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Server;
+using ESRI.Server.SOESupport;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+
+//This is REST SOE template of Enterprise SDK
+
+//TODO: sign the project (project properties > signing tab > sign the assembly)
+//      this is strongly suggested if the dll will be registered using regasm.exe <your>.dll /codebase
+
+
+namespace RestProSOE1
+{
+    [ComVisible(true)]
+    [Guid("16a9c9d3-488d-442c-ab18-002e5807d9c2")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ServerObjectExtension("MapServer",
+        AllCapabilities = "",
+        DefaultCapabilities = "",
+        Description = "",
+        DisplayName = "RestProSOE1",
+        Properties = "",
+        SupportsREST = true,
+        SupportsSOAP = false,
+        SupportsSharedInstances = false)]
+    public class RestProSOE1 : IServerObjectExtension, IObjectConstruct, IRESTRequestHandler
+    {
+        private string soe_name;
+
+        private IPropertySet configProps;
+        private IServerObjectHelper serverObjectHelper;
+        private ServerLogger logger;
+        private IRESTRequestHandler reqHandler;
+
+        public RestProSOE1()
+        {
+            soe_name = this.GetType().Name;
+            logger = new ServerLogger();
+            reqHandler = new SoeRestImpl(soe_name, CreateRestSchema()) as IRESTRequestHandler;
+        }
+
+        #region IServerObjectExtension Members
+
+        public void Init(IServerObjectHelper pSOH)
+        {
+            serverObjectHelper = pSOH;
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        #endregion
+
+        #region IObjectConstruct Members
+
+        public void Construct(IPropertySet props)
+        {
+            configProps = props;
+        }
+
+        #endregion
+
+        #region IRESTRequestHandler Members
+
+        public string GetSchema()
+        {
+            return reqHandler.GetSchema();
+        }
+
+        public byte[] HandleRESTRequest(string Capabilities, string resourceName, string operationName, string operationInput, string outputFormat, string requestProperties, out string responseProperties)
+        {
+            return reqHandler.HandleRESTRequest(Capabilities, resourceName, operationName, operationInput, outputFormat, requestProperties, out responseProperties);
+        }
+
+        #endregion
+
+        private RestResource CreateRestSchema()
+        {
+            RestResource rootRes = new RestResource(soe_name, false, RootResHandler);
+
+            RestOperation sampleOper = new RestOperation("sampleOperation",
+                                                      new string[] { "parm1", "parm2" },
+                                                      new string[] { "json" },
+                                                      SampleOperHandler);
+
+            rootRes.operations.Add(sampleOper);
+
+            return rootRes;
+        }
+
+        private byte[] RootResHandler(NameValueCollection boundVariables, string outputFormat, string requestProperties, out string responseProperties)
+        {
+            responseProperties = null;
+
+            var json = JsonSerializer.Serialize(
+                new
+                {
+                    hello = "world"
+                });
+
+            return Encoding.UTF8.GetBytes(json);
+        }
+
+        private byte[] SampleOperHandler(NameValueCollection boundVariables,
+                                                  JsonObject operationInput,
+                                                      string outputFormat,
+                                                      string requestProperties,
+                                                  out string responseProperties)
+        {
+            responseProperties = null;
+
+            string parm1Value;
+            bool found = operationInput.TryGetString("parm1", out parm1Value);
+            if (!found || string.IsNullOrEmpty(parm1Value))
+                throw new ArgumentNullException("parm1");
+
+            string parm2Value;
+            found = operationInput.TryGetString("parm2", out parm2Value);
+            if (!found || string.IsNullOrEmpty(parm2Value))
+                throw new ArgumentNullException("parm2");
+
+            var result = new
+            {
+                parm1 = parm1Value,
+                parm2 = parm2Value
+            };
+            var json = JsonSerializer.Serialize(result);
+
+            return Encoding.UTF8.GetBytes(json);
+        }
+
+
+    }
+}
