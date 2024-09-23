849a1f28ff148e0468fbee514b61568fcabd60d9
Robert Blischke <rblischke@esri.com>
2024-09-23 15:39:18 -0700

    initial check in

---
+++ react-test-app-config/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import data from './data.json'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div >
+        {
+          data.map((data,i) => (
+            <div key={i}>
+                <h1>{data.title}</h1>
+                <p>{data.url}</p>
+            </div>
+          ))
+        }
+      </div>
+      </header>
+      
+    </div>
+    
+    
+  );
+}
+
+export default App;
