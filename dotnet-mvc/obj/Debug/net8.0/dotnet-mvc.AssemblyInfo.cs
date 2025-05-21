68e99f058caffadff3cabc5aa2657b20fed8856f
Robert Blischke <rblischke@esri.com>
2025-05-21 13:28:47 -0700

    Update elastic beanstalk application and environment

--- dotnet-mvc/obj/Debug/net8.0/dotnet-mvc.AssemblyInfo.cs
+++ dotnet-mvc/obj/Debug/net8.0/dotnet-mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e7a11146b02757b9250ceff767dee6df18791c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57073165d0992ce800d2c2a0a0104a8ce72713e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
